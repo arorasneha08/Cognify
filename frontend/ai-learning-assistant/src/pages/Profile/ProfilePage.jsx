5dfb60b2352f691fd3ca038323789d560f3b3df5
Sneha <snehaarora68791@gmail.com>
2026-04-07 15:24:43 +0530

    User Profile Page created

--- frontend/ai-learning-assistant/src/pages/Profile/ProfilePage.jsx
+++ frontend/ai-learning-assistant/src/pages/Profile/ProfilePage.jsx
@@ -1,9 +1,137 @@
-import React from 'react'
+import { useState , useEffect, use } from "react"
+import PageHeader from "../../components/common/PageHeader";
+import Button from "../../components/common/Button";
+import Spinner from "../../components/common/Spinner";
+import authService from "../../services/AuthService";
+import {useAuth} from "../../context/AuthContext"
+import toast from 'react-hot-toast'
+import {User , Mail , Lock} from 'lucide-react'; 
 
 const ProfilePage = () => {
+  const [loading , setLoading] = useState(false);
+  const [passwordLoading , setPasswordLoading] = useState(false);
+  const [username , setUsername] = useState('');
+  const [email , setEmail] = useState('');
+  const [currentPassword , setCurrentPassword] = useState('');
+  const [newPassword , setNewPassword] = useState('');
+  const [confirmPassword , setConfirmPassword] = useState('');
+
+  useEffect(() => {
+    const fetchProfile = async() => {
+      try{
+        const {data} = await authService.getProfile();
+        setUsername(data.username);
+        setEmail(data.email);
+      }
+      catch(error){
+        toast.error("Failed to fetch profile");
+        console.log(error);
+      }
+      finally{
+        setLoading(false);
+      }
+    } ;
+    fetchProfile(); 
+  }, []); 
+
+  const handleChangePassword = async(e) => {
+    e.preventDefault(); 
+    if(newPassword !== confirmPassword){
+      toast.error("New Password and Confirm Password do not match.");
+      return;
+    }
+
+    if(newPassword.length < 6){
+      toast.error("Password must be at least 6 characters.");
+      return;
+    }
+    setPasswordLoading(true);
+    try{
+      await authService.changePassword({currentPassword , newPassword});
+      toast.success("Password changed successfully");
+      setNewPassword(""); 
+      setCurrentPassword("");
+      setConfirmPassword("");
+    }
+    catch(error){
+      toast.error("Failed to change password");
+      console.log(error);
+    }
+    finally{
+      setPasswordLoading(false);
+    }
+  };
+
+  if(loading){
+    return <Spinner />
+  }
+
   return (
     <div>
-      profile page 
+      <PageHeader title="Profile Settings"/>
+      <div className="space-y-8">
+        {/* User information display */}
+        <div className="bg-white border border-neutral-200 rounded-lg p-6" >
+          <h3 className="text-lg font-semibold text-neutral-900 mb-4">User Information</h3>
+          <div className="space-y-4">
+            <div>
+              <label className="block text-xs font-medium text-neutral-700 mb-1.5">Username</label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <User className="h-4 w-4 text-neutral-400" />
+                </div>
+                <p className="w-full h-9 pl-9 pr-3 pt-2 border border-neutral-200 rounded-lg bg-neutral-50 text-sm text-neutral-900">{username}</p>
+              </div>
+            </div>
+          </div>
+          <label className="block text-xs font-medium text-neutral-700 mb-1.5 mt-2">Email Address</label>
+          <div className="relative">
+            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+              <Mail className="h-4 w-4 text-neutral-400"/>
+            </div>
+            <p className="w-full h-9 pl-9 pr-3 pt-2 border border-neutral-200 rounded-lg bg-neutral-50 text-sm text-neutral-900">{email}</p>
+          </div>
+        </div>
+
+        {/* change password */}
+        <div className="bg-white border border-neutral-200 rounded-lg p-6">
+          <h3 className="text-lg font-semibold text-neutral-900 mb-4">Change Password </h3>
+
+          <form onSubmit={handleChangePassword} className="space-y-4">
+            <div>
+              <label className="block text-xs font-medium text-neutral-700 mb-1.5">Current Password</label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <Lock className="h-4 w-4 text-neutral-400"/>
+                </div>
+                <input type="password" value={currentPassword} onChange={(e) => setCurrentPassword(e.target.value)} required className="w-full h-9 pl-9 pr-3 border border-neutral-200 rounded-lg bg-white text-sm text-neutral-900 placeholder:text-neutral-400 transition-transform duration-20 focus:outline-none focus:ring-2 hover:ring-[#00d492] focus:border-transparent"/>
+              </div>
+            </div>
+            <div>
+              <label className="block text-xs font-medium text-neutral-700 mb-1.5">New Password</label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <Lock className="h-4 w-4 text-neutral-400"/>
+                </div>
+                <input type="password" className="w-full h-9 pl-9 pr-3 border border-neutral-200 rounded-lg bg-white text-sm text-neutral-900 placeholder:text-neutral-400 transition-colors duration-150 focus:outline-none focus:ring-2 hover:ring-[#00d492] focus:border-transparent" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} required/>
+              </div>
+            </div>
+            <div className="block text-xs font-medium text-neutral-700 mb-1.5">
+              <label className="block text-xs font-medium text-neutral-700 mb-1.5">Confirm New Password</label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <Lock className="h-4 w-4 text-neutral-400"/>
+                </div>
+                <input className="w-full h-9 pl-9 pr-3 border border-neutral-200 rounded-lg bg-white text-sm text-neutral-900 placeholder:text-neutral-400 transition-colors duration-150 focus:outline-none focus:ring-2 hover:ring-[#00d492] focus:border-transparent" type="password" onChange={(e) => setConfirmPassword(e.target.value)} required/>
+              </div>
+            </div>
+
+            <div className="flex justify-end mt-5">
+              <Button type="submit" className="cursor-pointer" disabled={passwordLoading}>{passwordLoading ? "Changing..." : "Change Password"}</Button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
